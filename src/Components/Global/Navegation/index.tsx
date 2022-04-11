b73cebf68b2cdb1a61fb68461a0dea38a7537028
JoanAttiLozano <jlozano@trainingroots.xyz>
2022-04-11 15:37:59 -0600

    nav v1

--- src/Components/Global/Navegation/index.tsx
+++ src/Components/Global/Navegation/index.tsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import logoKuro from "Assets/Images/logoKuro.svg";
 
-function Navegation() {
+import { bindActionCreators } from "redux";
+import { ThemeCreators } from "Storage";
+import { useDispatch, useSelector } from "react-redux";
+import { StateReducer } from "Interfaces/Reducer";
+import { useNavigate } from "react-router-dom";
+import styles from "./Navegation.module.css";
+
+const Navegation: React.FC = () => {
+  const dispatch = useDispatch();
+  const { theme } = useSelector((state: StateReducer) => state.theme);
+  const { changeTheme } = bindActionCreators(ThemeCreators, dispatch);
+  const navigation = useNavigate();
+
+  const handleClickImage = () => navigation("/");
+
+  const handleChangeTheme = () => {
+    if (theme === "dark") {
+      changeTheme({
+        theme: "light",
+      });
+    } else {
+      changeTheme({
+        theme: "dark",
+      });
+    }
+  };
+
   return (
-    <div>Navegation</div>
-  )
-}
+    <nav className={[styles.nav, "shadow"].join("")}>
+      {/* Logo  */}
+      <img
+        src={logoKuro}
+        alt="Kuro Design"
+        className={styles.img}
+        onClick={handleClickImage}
+      />
+      <div className={styles.containerModes}>
+        <div className={styles.modes}>
+          <label className={styles.appChange}> Change Theme</label>
+          <input className={styles.switch} type="checkbox" id="switch" />
+          <label
+            className={styles.switchclick}
+            htmlFor="switch"
+            onClick={handleChangeTheme}
+          >
+            Change {theme} theme
+          </label>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-export default Navegation
+export default Navegation;
