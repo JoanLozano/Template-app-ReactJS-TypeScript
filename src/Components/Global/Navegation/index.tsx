44f46016c48e633c1246c39ef97240600f415851
JoanAttiLozano <jlozano@trainingroots.xyz>
2022-04-11 16:20:23 -0600

    Nav Update 2

--- src/Components/Global/Navegation/index.tsx
+++ src/Components/Global/Navegation/index.tsx
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
 import logoKuro from "Assets/Images/logoKuro.svg";
-
 import { bindActionCreators } from "redux";
 import { ThemeCreators } from "Storage";
 import { useDispatch, useSelector } from "react-redux";
 import { StateReducer } from "Interfaces/Reducer";
 import { useNavigate } from "react-router-dom";
 import styles from "./Navegation.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
 
 const Navegation: React.FC = () => {
   const dispatch = useDispatch();
@@ -29,17 +30,20 @@ const Navegation: React.FC = () => {
   };
 
   return (
-    <nav className={[styles.nav, "shadow"].join("")}>
-      {/* Logo  */}
-      <img
-        src={logoKuro}
-        alt="Kuro Design"
-        className={styles.img}
-        onClick={handleClickImage}
-      />
-      <div className={styles.containerModes}>
-        <div className={styles.modes}>
-          <label className={styles.appChange}> Change Theme</label>
+    <div className={styles.gradient}>
+      <div className={styles.container}>
+        <div className={styles.navegationLogo}>
+          <h1>Kuro Design</h1>
+        </div>
+        <div className={styles.navegationLinks}>
+          <div className={styles.containerSwitch}>
+            <FontAwesomeIcon
+              icon={faSun}
+              size="lg"
+              style={{ color: "white" }}
+              className={styles.icon}
+            />
+          </div>
           <input className={styles.switch} type="checkbox" id="switch" />
           <label
             className={styles.switchclick}
@@ -48,9 +52,17 @@ const Navegation: React.FC = () => {
           >
             Change {theme} theme
           </label>
+          <div className={styles.containerSwitch}>
+            <FontAwesomeIcon
+              icon={faMoon}
+              size="lg"
+              style={{ color: "white" }}
+              className={styles.icon}
+            />
+          </div>
         </div>
       </div>
-    </nav>
+    </div>
   );
 };
 
